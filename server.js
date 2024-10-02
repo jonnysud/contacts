0d0656917b27778c10d980203b7cdd9565302df5
jonnysud <anzianoponce11@gmail.com>
2024-10-02 10:35:17 -0500

    added api docs

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const mongodb = require('./data/databases');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE,OPTIONS');
+    next();
+})
 app.use('/', require('./routes'));
 
 
