feb5fad6badca538c36d28951779bbc4a70b2b38
jonnysud <anzianoponce11@gmail.com>
2024-10-07 23:50:03 -0500

    Last updated

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const usersController = require("../controllers/users");
+const validateContact = require('../middleware/validate');
 
 router.get('/', usersController.getAll); 
 
-router.get('/:id', usersController.getSingle);
+router.get('/:id',usersController.getSingle);
 
-router.post('/', usersController.createUser);
+router.post('/', validateContact.saveContact, usersController.createUser);
 
-router.put('/:id', usersController.updateUser);
+router.put('/:id', validateContact.saveContact, usersController.updateUser);
 
 router.delete('/:id', usersController.deleteUser);
 
